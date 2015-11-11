7c67f5212632aadfc27cfd36599b52617c5e73b2
LeoMelo87 <lanm.audio@gmail.com>
2015-11-11 10:19:12 +0000

    Fonts added

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
 
-
-
-
-      $("button").click(function(){
-
-        $(".mainmenu").slideToggle();
-
-      });
-
-
-
-
+// WHEN IN MOBILE VERSION ADD THE HAMBURGER MENU BUTTON
+if ($(window).width() < 500) {
+  $("button").add()
+}
+// IF THE WINDOW WIDTH IS OVER 500PX, REMOVE THE BUTTON
+else {
+  $("button").remove()
+  $("gallery js-flickity").remove('"resize": true')
+}
 
 });
+
+// CLICK ON HAMBURGER MENU BUTTON
+$("button").click(function(){
+  // TOGGLE NAV MENU
+  $(".mainmenu").toggle();
+})
